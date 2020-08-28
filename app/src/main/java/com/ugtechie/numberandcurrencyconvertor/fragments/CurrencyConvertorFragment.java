97c20e7e02f30f1db0ca4a65afb5dc2283e249c8
OdagaJosh <34144362+odaga@users.noreply.github.com>
2020-08-28 13:33:39 +0300

    Added lottie animation on the first activty

--- app/src/main/java/com/ugtechie/numberandcurrencyconvertor/fragments/CurrencyConvertorFragment.java
+++ app/src/main/java/com/ugtechie/numberandcurrencyconvertor/fragments/CurrencyConvertorFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +15,35 @@ import androidx.fragment.app.Fragment;
 
 import com.ugtechie.numberandcurrencyconvertor.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrencyConvertorFragment extends Fragment {
+    private Spinner spinner1, spinner2;
+    private EditText from;
+    private TextView to;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_currency_converter, container, false);
+        View v = inflater.inflate(R.layout.fragment_currency_converter, container, false);
+
+        Button btnSubmit = v.findViewById(R.id.btnSubmit);
+        from = v.findViewById(R.id.InputEditText);
+        to = v.findViewById(R.id.OutputTextView);
+        spinner1 = v.findViewById(R.id.spinner1);
+
+        List<String> list1 = new ArrayList<String>();
+        list1.add("United States Dollar");
+        list1.add("Armenian Dram");
+        list1.add("Bitcoin");
+        list1.add("Chinese Yuan");
+        list1.add("Indian Rupee");
+        list1.add("Kuwaiti Dinar");
+        list1.add("Mexican Peso");
+        list1.add("Omani Rial");
+        list1.add("Bangladeshi Taka");
+
+        return v;
     }
 }
